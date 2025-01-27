faf85727587b4fa68ad1188dd3ab6becc0aa7004
KannJedsada <133192794+KannJedsada@users.noreply.github.com>
2025-01-27 02:41:14 +0000

    loading logi ldfadfsdfddflo

--- client/src/components/auth/login.js
+++ client/src/components/auth/login.js
@@ -88,9 +88,39 @@ const Login = () => {
           </div>
           <button
             type="submit"
-            className="w-full px-4 py-2 bg-blue-600 text-white font-semibold rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+            className={`px-6 py-2 text-white rounded-lg shadow-md ${isLoading
+              ? "bg-gray-400 cursor-not-allowed"
+              : "bg-blue-700 hover:bg-blue-600"
+              }`}
+            disabled={isLoading}
           >
-            เข้าสู่ระบบ
+            {isLoading ? (
+              <div className="flex items-center">
+                <svg
+                  className="animate-spin h-5 w-5 text-white mr-2"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  ></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+                  ></path>
+                </svg>
+                กำลังเข้าสู่ระบบ...
+              </div>
+            ) : (
+              "เข้าสู่ระบบ"
+            )}
           </button>
           <div className="flex items-center justify-center">
             <button
--- client/src/components/auth/loginSystem.js
+++ client/src/components/auth/loginSystem.js
@@ -8,11 +8,13 @@ const LoginSystem = () => {
   const [idCard, setIdCard] = useState("");
   const [email, setEmail] = useState("");
   const { login } = useContext(AuthContext);
+  const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
 
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
+      setIsLoading(true);
       const response = await axios.post("/api/login", {
         id: idCard,
         email: email,
@@ -56,6 +58,8 @@ const LoginSystem = () => {
         });
         console.error("Login failed", error.message);
       }
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -100,9 +104,39 @@ const LoginSystem = () => {
           </div>
           <button
             type="submit"
-            className="w-full px-4 py-2 bg-blue-600 text-white font-semibold rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+            className={`px-6 py-2 text-white rounded-lg shadow-md ${isLoading
+              ? "bg-gray-400 cursor-not-allowed"
+              : "bg-blue-700 hover:bg-blue-600"
+              }`}
+            disabled={isLoading}
           >
-            เข้าสู่ระบบ
+            {isLoading ? (
+              <div className="flex items-center">
+                <svg
+                  className="animate-spin h-5 w-5 text-white mr-2"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  ></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+                  ></path>
+                </svg>
+                กำลังเข้าสู่ระบบ...
+              </div>
+            ) : (
+              "เข้าสู่ระบบ"
+            )}
           </button>
           <div className="flex items-center justify-center">
             <button
@@ -115,7 +149,7 @@ const LoginSystem = () => {
           </div>
         </form>
       </div>
-    </div>
+    </div >
   );
 };
 
