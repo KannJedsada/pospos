6656dab4601f3bf9a74174f055302f64613d614b
KannJedsada <133192794+KannJedsada@users.noreply.github.com>
2025-01-27 03:32:05 +0000

    edit authcontext

--- client/src/components/auth/authcontext.js
+++ client/src/components/auth/authcontext.js
@@ -46,18 +46,25 @@
 
 // export default AuthContext;
 import React, { createContext, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "../../utils/axiosInstance";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
+  const navigate = useNavigate();
   const [authData, setAuthData] = useState(() => {
     const token = localStorage.getItem("token");
     const id_card = localStorage.getItem("id_card");
     return token && id_card ? { token, id_card } : {};
   });
-  const [access, setAccess] = useState(null);
+  const [access, setAccess] = useState(() => {
+    // ดึงค่า access จาก LocalStorage เมื่อเริ่มต้น
+    const savedAccess = localStorage.getItem("access");
+    return savedAccess ? JSON.parse(savedAccess) : null;
+  });
 
+  // เช็ก token หมดอายุ
   useEffect(() => {
     const checkTokenExpiration = () => {
       const token = localStorage.getItem("token");
@@ -71,16 +78,13 @@ export const AuthProvider = ({ children }) => {
     };
 
     checkTokenExpiration();
-    const interval = setInterval(checkTokenExpiration, 60000);
+    const interval = setInterval(checkTokenExpiration, 60000); // เช็กทุก 60 วินาที
     return () => clearInterval(interval);
   }, []);
 
+  // ดึงข้อมูล access เมื่อ authData เปลี่ยนแปลง
   useEffect(() => {
     const fetchData = async () => {
-      if (window.location.pathname === "/login") {
-        return;
-      }
-
       if (authData.token && authData.id_card) {
         try {
           const res = await axios.get(`/api/emp/data/${authData.id_card}`, {
@@ -88,14 +92,20 @@ export const AuthProvider = ({ children }) => {
               Authorization: `Bearer ${authData.token}`,
             },
           });
-          setAccess(res.data.data[0].access);
+          const userAccess = res.data.data[0].access;
+          setAccess(userAccess);
+          localStorage.setItem("access", JSON.stringify(userAccess)); // เก็บ access ใน LocalStorage
         } catch (error) {
           console.error("Failed to fetch access data", error);
+          logout(); // หากเกิดปัญหา เช่น token ไม่ถูกต้อง ให้ลบข้อมูลและไปหน้า login
         }
+      } else {
+        navigate("/login");
       }
     };
+
     fetchData();
-  }, [authData]);
+  }, [authData, navigate]);
 
   const login = (token, id_card) => {
     localStorage.setItem("token", token);
@@ -106,8 +116,10 @@ export const AuthProvider = ({ children }) => {
   const logout = () => {
     localStorage.removeItem("token");
     localStorage.removeItem("id_card");
+    localStorage.removeItem("access"); // ลบ access ออกจาก LocalStorage
     setAuthData({});
     setAccess(null);
+    navigate("/login");
   };
 
   return (
