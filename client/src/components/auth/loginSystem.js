729a023d0547b27b0f185ced955a9592c22e3a1f
KannJedsada <133192794+KannJedsada@users.noreply.github.com>
2025-01-27 02:54:48 +0000

    loading login

--- client/src/components/auth/loginSystem.js
+++ client/src/components/auth/loginSystem.js
@@ -120,42 +120,45 @@ const LoginSystem = () => {
               className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             />
           </div>
-          <button
-            type="submit"
-            className={`px-6 py-2 text-white w-100 rounded-lg shadow-md flex items-center justify-center ${isLoading
-              ? "bg-gray-400 cursor-not-allowed"
-              : "bg-blue-700 hover:bg-blue-600"
-              }`}
-            disabled={isLoading}
-          >
-            {isLoading ? (
-              <div className="flex items-center">
-                <svg
-                  className="animate-spin h-5 w-5 text-white mr-2"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <circle
-                    className="opacity-25"
-                    cx="12"
-                    cy="12"
-                    r="10"
-                    stroke="currentColor"
-                    strokeWidth="4"
-                  ></circle>
-                  <path
-                    className="opacity-75"
-                    fill="currentColor"
-                    d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
-                  ></path>
-                </svg>
-                กำลังเข้าสู่ระบบ...
-              </div>
-            ) : (
-              "เข้าสู่ระบบ"
-            )}
-          </button>
+          <div className="flex items-center justify-center min-h-screen bg-gray-100">
+            <button
+              type="submit"
+              className={`px-6 py-2 text-white rounded-lg flex items-center justify-center shadow-md ${isLoading
+                ? "bg-gray-400 cursor-not-allowed"
+                : "bg-blue-700 hover:bg-blue-600"
+                }`}
+              disabled={isLoading}
+            >
+              {isLoading ? (
+                <div className="flex items-center">
+                  <svg
+                    className="animate-spin h-5 w-5 text-white mr-2"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      strokeWidth="4"
+                    ></circle>
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+                    ></path>
+                  </svg>
+                  กำลังเข้าสู่ระบบ...
+                </div>
+              ) : (
+                "เข้าสู่ระบบ"
+              )}
+            </button>
+          </div>
+
           <div className="flex items-center justify-center">
             <button
               type="button"
