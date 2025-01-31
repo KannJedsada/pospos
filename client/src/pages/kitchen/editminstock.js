2836a629f8ca712d0ff237444074ae30153304cb
KannJedsada <133192794+KannJedsada@users.noreply.github.com>
2025-01-31 13:12:49 +0000

    edit edit min stock page

--- client/src/pages/kitchen/editminstock.js
+++ client/src/pages/kitchen/editminstock.js
@@ -78,7 +78,6 @@ function Editminstock() {
             className="flex items-center text-white bg-blue-700 hover:bg-blue-600 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-4 shadow-md transition-all"
           >
             <ChevronLeft className="mr-2" />
-            กลับ
           </button>
         </div>
 
