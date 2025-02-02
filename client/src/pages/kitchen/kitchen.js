ea14e1403017d5abedb2b02547abb262c7e547a1
KannJedsada <133192794+KannJedsada@users.noreply.github.com>
2025-02-02 07:39:15 +0000

    edit kitchen

--- client/src/pages/kitchen/kitchen.js
+++ client/src/pages/kitchen/kitchen.js
@@ -45,10 +45,10 @@ function Kitchen() {
 
       // ส่งคำขอ PUT สำหรับออร์เดอร์ที่กรองมา
       if (newStatus === 3) {
-        console.log(ordersToUpdate?.t_name)
-        console.log(ordersToUpdate?.menu_name)
-        console.log(ordersToUpdate?.qty)
-        // console.log(ordersToUpdate);
+        // console.log(ordersToUpdate?.t_name)
+        // console.log(ordersToUpdate?.menu_name)
+        // console.log(ordersToUpdate?.qty)
+        console.log(ordersToUpdate);
       }
 
       // await Promise.all(
