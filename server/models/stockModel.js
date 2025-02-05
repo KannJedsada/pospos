be1eb175b6fd593b402d484d0db731a8eeab88d8
KannJedsada <133192794+KannJedsada@users.noreply.github.com>
2025-02-05 17:39:49 +0000

    edit new stock

--- server/models/stockModel.js
+++ server/models/stockModel.js
@@ -138,10 +138,9 @@ FROM materials;`);
 
         for (let comp of composite.rows) {
           const { material_id: comp_mat_id, quantity_used: comp_qty, unit_id } = comp;
-          console.log(comp);
-          const unit_from = await pool.query(`SELECT unit FROM materials WHERE id = $1`, [comp_mat_id]);
-          console.log(unit_from.rows[0]);
-          const converUnit = await pool.query(`SELECT * FROM unit_conversions WHERE from_unit_id = $1 AND to_unit_id = $2`,[unit_id, unit_from]);
+          const unitResult = await pool.query('SELECT unit FROM materials WHERE id = $1', [materialId]);
+          const unit = unitResult.rows[0]?.unit;
+          const converUnit = await pool.query(`SELECT * FROM unit_conversions WHERE from_unit_id = $1 AND to_unit_id = $2`, [unit_id, unit]);
           console.log(converUnit.rows[0]);
           // const qty_comp = comp_qty * qty;
 
